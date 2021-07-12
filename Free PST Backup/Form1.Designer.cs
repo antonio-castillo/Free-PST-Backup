c352f080cd3bf4fa683e6b41a2beb9b29672b8a8
antonio-castillo <a.castillomoreno92@gmail.com>
2021-07-12 09:21:17 +0200

    Agregar archivos de proyecto.

---
+++ Free PST Backup/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace Free_PST_Backup
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkedListBox = new System.Windows.Forms.CheckedListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Free_PST_Backup.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(97, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Free PST";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(119, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 51);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Scan";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkedListBox
+            // 
+            this.checkedListBox.BackColor = System.Drawing.Color.Silver;
+            this.checkedListBox.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkedListBox.FormattingEnabled = true;
+            this.checkedListBox.Location = new System.Drawing.Point(119, 24);
+            this.checkedListBox.Name = "checkedListBox";
+            this.checkedListBox.Size = new System.Drawing.Size(516, 204);
+            this.checkedListBox.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(511, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 51);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Backup";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButton1.ForeColor = System.Drawing.Color.White;
+            this.radioButton1.Location = new System.Drawing.Point(206, 229);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(109, 25);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "User Profile";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.radioButton2.ForeColor = System.Drawing.Color.White;
+            this.radioButton2.Location = new System.Drawing.Point(321, 229);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(108, 25);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.Text = "Anywhere";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(115, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 21);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Search in:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Yellow;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 189);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(92, 15);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Antonio Castillo";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(119, 266);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(516, 23);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(511, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 21);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Total PST:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(12, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 24);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Backup";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(644, 362);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkedListBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Free PST Backup";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckedListBox checkedListBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Free PST Backup/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Free_PST_Backup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+
+        String currectDirectory = Directory.GetCurrentDirectory();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showMessage("This process may take a while \n Please, don't close the application \n This message will close automatically in 4 seconds", 4000);  /* 1 segundo = 1000 */
+            progressBar1.Value = 0;
+
+            if (radioButton1.Checked)
+            {
+                String userprofile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+                checkedListBox.Items.Clear();
+                List<string> file_list = new List<string>();
+                SearchDirectory(userprofile, file_list);
+
+                label3.Text = "Total PST: " + checkedListBox.Items.Count.ToString();
+
+
+            }
+            else if (radioButton2.Checked)
+            {
+                checkedListBox.Items.Clear();
+                List<string> file_list = new List<string>();
+                SearchDirectory(@"c:\\", file_list);
+
+                label2.Text = "Total PST: " + checkedListBox.Items.Count.ToString();
+            }
+
+
+            if (checkedListBox.Items.Count <= 0)
+            {
+                MessageBox.Show("No PST files found");
+            }
+
+
+
+        }
+
+
+        private void SearchDirectory(string path, List<string> file_list)
+        {
+            DirectoryInfo dir_info = new DirectoryInfo(path);
+            try
+            {
+                foreach (DirectoryInfo subdir_info in dir_info.GetDirectories())
+                {
+                    SearchDirectory(subdir_info.FullName, file_list);
+                }
+            }
+            catch
+            {
+            }
+            try
+            {
+
+                foreach (FileInfo file_info in dir_info.GetFiles())
+                {
+                    if (file_info.Extension == ".pst" || file_info.Extension == ".ost")
+                        checkedListBox.Items.Add(file_info);
+                }
+
+            }
+            catch
+            {
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            progressBar1.Value = 0;
+
+
+            //Cerrar Outlook antes de copiar
+            killOutlook();
+
+            // Determine if there are any items checked.  
+            if (checkedListBox.CheckedItems.Count != 0)
+            {
+
+
+                showMessage("This process may take a while \n Please, don't close the application \n This message will close automatically in 4 seconds", 4000);  /* 1 segundo = 1000 */
+
+                Directory.CreateDirectory(DateTime.Now.ToString("[yy-MM-dd]" + " PST Backup"));
+                // If so, loop through all checked items and print results.  
+
+                string dest_path = DateTime.Now.ToString("[yy-MM-dd]" + " PST Backup");
+                for (int i = 0; i < checkedListBox.CheckedItems.Count; i++)
+                {
+
+                    string path_item = Path.GetFullPath(checkedListBox.CheckedItems[i].ToString());
+                    string path_only = Path.GetDirectoryName(checkedListBox.CheckedItems[i].ToString());
+                    String file_name = Path.GetFileName(checkedListBox.CheckedItems[i].ToString());
+                    string path_folderDate = dest_path + "/" + file_name;
+
+                    //Meter condicion si existe archivo
+                    if (File.Exists(path_folderDate))
+                    {
+                        count++;
+                        FileInfo file = new FileInfo(path_item);
+                        file.CopyTo(dest_path + "/" + "(" + count + ")" + file_name);
+
+
+                    }
+                    else
+                    {
+                        FileInfo file = new FileInfo(path_item);
+                        file.CopyTo(dest_path + "/" + file_name);
+                    }
+
+                }
+                //cuando se termina la copia se llena la barra indicando el proceso completado
+                progressBar1.Value = 1;
+
+
+                //abre la ubicacion
+                Process.Start("explorer.exe", dest_path );
+            }
+
+
+            else
+            {
+                MessageBox.Show("You must select a PST");
+            }
+        }
+
+        private void killOutlook()
+        {
+
+            //Comprobar si el proceso Outlook.exe esta corriendo y si no lo esta, lo cierra.
+
+            Process[] proc = System.Diagnostics.Process.GetProcessesByName("OUTLOOK");
+
+            if (proc.Length > 0)
+            {
+                proc[0].Kill();
+            }
+            else
+            {
+                //Nothing to do
+            }
+        }
+
+
+        private void timeTick(object sender, EventArgs e)
+        {
+            (sender as Timer).Stop();  /* Detiene el Timer */
+            SendKeys.Send("{ESC}"); /* Hace la simulación de la tecla Escape, también puedes usar {ENTER} */
+        }
+
+
+        private void showMessage(string msg, int duration)
+        {
+            using (Timer t = new Timer())
+            {
+                Timer time = new Timer();
+                time.Interval = duration;
+                time.Tick += timeTick;  /* Evento enlazado */
+
+                time.Start();
+
+                /* Muestras el texto en el MB */
+                MessageBox.Show(msg);
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/antonio-castillo");
+        }
+    }
+}
